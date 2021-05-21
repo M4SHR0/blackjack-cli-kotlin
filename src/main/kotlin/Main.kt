aeb0d8b7b8b9030daf989d60c98e6eae4944e85e
mashro <fall.est.mori@gmail.com>
2021-05-21 18:18:14 +0900

    add test comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 fun main(){
     val card = Card()
     val getcard = card.getCard()
-    println(getcard)
+    println("想定getcard" + getcard)
     val shuffle = card.shuffle()
-    println(shuffle)
+    println("想定shuffle" + shuffle)
     val giveout = card.giveOut(2,2,  shuffledDeck = shuffle)
-    println(giveout)
+    println("想定giveout" + giveout)
 
     // TODO 挙動がおかしいVer.
-    println(getcard)
+    println("異常getcard" + getcard)
+    println("異常shuffle" + shuffle)
+    println("異常giveout" + giveout)
+
+    // TODO 異常なはずの検証用
     println(shuffle)
-    println(giveout)
 }
