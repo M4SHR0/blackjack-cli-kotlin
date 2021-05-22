0336d107a0b9e561bd99bc2e52e87df3dbba5a98
m4shr0 <fall.est.mori@gmail.com>
2021-05-22 19:41:50 +0900

    refactoring all class

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,14 +1,29 @@
-class Game {
-    fun calcScore(deck:MutableList<Int>): Int {
+class Game(playerCount: Int,cardCount: Int) {
+    val card = Card()
+    val getcard = card.getCard()
+    val shuffle = card.shuffle(getcard)
+    val giveout = card.giveOut(pCount = playerCount,cCount = cardCount,  recieveDeck = shuffle)
+
+    fun calcScore(){
         var score = 0
-        for(essential in deck){
-            when(essential%13){
-                in 1..8 -> score += essential%13+1
-                in 10..12 -> score += 10
-                0 -> if(score + 11 <= 21) score += 11 else score += 1
-                9 -> if(score + 11 <= 21) score += 11 else score += 1
+        for(oneDeck in giveout){
+            for(essential in oneDeck){
+                when(essential%13){
+                    in 1..8 -> score += essential%13+1
+                    in 10..12 -> score += 10
+                    0 -> if(score + 11 <= 21) score += 11 else score += 1
+                    9 -> if(score + 11 <= 21) score += 11 else score += 1
+                }
             }
+            println("Score:$score")
         }
-        return score
+
+    }
+
+    fun checkDeck(){
+        // 確認用
+        println("getcard$getcard")
+        println("shuffle$shuffle")
+        println("giveout$giveout")
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
-fun main(){
-    val card = Card()
-    val getcard = card.getCard()
-    val shuffle = card.shuffle(getcard)
-    val giveout = card.giveOut(2,2,  recieveDeck = shuffle)
-
-    val game = Game()
-    for(i in giveout){
-        println("Score:" + game.calcScore(i))
-    }
-
-    // 確認用
-    println("getcard$getcard")
-    println("shuffle$shuffle")
-    println("giveout$giveout")
+fun main() {
+    val game = Game(2, 2)
+    game.checkDeck()
+    game.calcScore()
 }
