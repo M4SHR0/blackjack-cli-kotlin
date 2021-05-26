815c609d9743928babc9718e879db4df3dfa4bcb
m4shr0 <fall.est.mori@gmail.com>
2021-05-27 06:57:34 +0900

    スコアの修正

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -9,10 +9,9 @@ class Game(playerCount: Int,cardCount: Int) {
         for(oneDeck in giveout){
             for(essential in oneDeck){
                 when(essential%13){
-                    in 1..8 -> score += essential%13+1
+                    in 1..9 -> score += essential%13+1
                     in 10..12 -> score += 10
                     0 -> if(score + 11 <= 21) score += 11 else score += 1
-                    9 -> if(score + 11 <= 21) score += 11 else score += 1
                 }
             }
             println("Score:$score")
